132ba38d619b98a84ce71d069942b9ffbbae6e6c
Daan <dhanrajappu456@gamail.com>
2023-07-28 12:38:08 +0530

    typo rectified in the main

--- src/designpattern/observer/Main.java
+++ src/designpattern/observer/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
         //simulating the subscription and the notification
         Subject weCode = new Publisher("weCode");
-        Observer amal = new Subscriber("Ronny");
+        Observer ronny = new Subscriber("Ronny");
         Observer robert = new Subscriber("Robert");
         Observer john = new Subscriber("John");
         Observer alex = new Subscriber("Alex");
         // registering all the users to the weCode channel
-        weCode.addObserver(amal);
+        weCode.addObserver(ronny);
         weCode.addObserver(robert);
         weCode.addObserver(john);
         weCode.addObserver(alex);
